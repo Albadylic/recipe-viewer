b3ce73e5a279f5c2e9af28fe7e6cb89cd9156594
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-12-18 15:59:52 +0000

    Move refactored analysis to actions file

--- app/api/analyse/actions.ts
+++ app/api/analyse/actions.ts
@@ -1,62 +1,28 @@
 "use server";
+import OpenAI from "openai";
 
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
-import { createStreamableValue } from "ai/rsc";
-// import dotenv from "dotenv";
-// dotenv.config();
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+export async function analyse(input: string) {
+  const prompt = `
+    Analysing this picture of food. Describe the visible food and drink items, list ingredients if detectable, and suggest the name for the dish if you can deduce it.`;
 
-// Analyse image
-export async function analyse(input: string | null) {
-  if (!input) {
-    return "No image provided";
-  }
-
-  try {
-    const apiKey = process.env.OPENAI_API_KEY;
-
-    if (!apiKey) {
-      throw new Error(
-        "The OPENAI_API_KEY environment variable is missing or empty. Please set it before running the script."
-      );
-    }
-
-    const stream = createStreamableValue();
-
-    (async () => {
-      const { textStream } = await streamText({
-        model: openai("gpt-4-turbo"),
-        messages: [
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: prompt },
           {
-            role: "user",
-            content: [
-              {
-                type: "text",
-                text: prompt,
-              },
-
-              {
-                type: "image",
-                image_url: {
-                  url: input,
-                },
-              },
-            ],
+            type: "image_url",
+            image_url: {
+              url: input,
+            },
           },
         ],
-      });
-
-      for await (const delta of textStream) {
-        stream.update(delta);
-      }
-
-      stream.done();
-    })();
-
-    return { response: stream.value };
-  } catch (error) {
-    return `Error: ${error.message}`;
-  }
+      },
+    ],
+  });
+  return response.choices[0].message.content;
 }
